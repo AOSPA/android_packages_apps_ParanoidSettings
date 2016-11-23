db0d0df8568b89b5c672181ecb391e2db9c6521f
Chris Crump <chris@cypheros.co>
2016-11-23 13:45:00 +0200

    ParanoidSettings: Forward Port Volume Long Press Skip Tracks [2/2]
    Bring Forward Volume Rocker Track Change
    
    * Thanks Sahul Krishan for the great animation
    * @ritujb: Fix default value mismatch with framework in playback control
    
    This squashes the following commit:
    
    Author: Akash <akashniki@gmail.com>
    Date:   Mon Dec 13 12:08:59 2021 +0100
    
        Settings: VolumeMusicControl: Use new material-you illustration preference
    
        * Switch to new Android 12 animation. Huge thanks to @aryanata for the animation
    
        Co-authored-by: Aryanata Andipradana <a.ryan.ata.ap@gmail.com>
        Change-Id: I75b300d46f2134e1957138dde130ee66cda6d9ee
    
    Change-Id: Ifa3d0987a0d54afd1f56e1d4a7488f428338c757
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -22,6 +22,7 @@ import android.hardware.display.AmbientDisplayConfiguration;
 import androidx.annotation.NonNull;
 
 import co.aospa.settings.gestures.SwipeToScreenshotPreferenceController;
+import co.aospa.settings.gestures.VolumeButtonMusicControlPreferenceController;
 
 import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -71,6 +72,7 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
                 .setConfig(ambientDisplayConfiguration));
         controllers.add(new PreventRingingParentPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new SwipeToScreenshotPreferenceController(context, FAKE_PREF_KEY));
+        controllers.add(new VolumeButtonMusicControlPreferenceController(context, FAKE_PREF_KEY));
         return controllers;
     }
 }
