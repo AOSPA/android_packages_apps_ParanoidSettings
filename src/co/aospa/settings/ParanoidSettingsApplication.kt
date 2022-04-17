3aa6a2345ffa40a8ce8ab715fb3016a4d6e7abe7
Anay Wadhera <awadhera@berkeley.edu>
2022-04-16 17:46:56 -0700

    ParanoidSettings: Initial cleaned import based on StatixOS
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Ie145f313df3ecf553fc4ae1077b82ca23253536a
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

---
+++ src/co/aospa/settings/ParanoidSettingsApplication.kt
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.settings
+
+import com.android.settings.SettingsApplication
+
+class ParanoidSettingsApplication : SettingsApplication() {
+}
