0dd361c78b342196acbaa6f14d0d390426043c4a
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-23 01:44:32 +0300

    ParanoidSettings: gestures: Add toggle for Screen-Off UDFPS
    Change-Id: I313969384fe605d7b5ff7dfc3a3fcebd514dd7cf
    
    Settings: Pull screen off UDFPS toggle out
    
    We don't need a whole page, a video and a suggestion pop-up for what's self explanatory in the summary
    Nuke leftovers
    
    Change-Id: Ia85a64708b557e1459ed5fcaedd2a600a38eb2ae

---
+++ src/co/aospa/settings/gestures/ScreenOffUdfpsPreferenceController.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.settings.gestures;
+
+import android.annotation.UserIdInt;
+import android.content.Context;
+import android.hardware.display.AmbientDisplayConfiguration;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import androidx.annotation.VisibleForTesting;
+
+import com.android.settings.core.TogglePreferenceController;
+import com.android.settings.R;
+
+public class ScreenOffUdfpsPreferenceController extends TogglePreferenceController {
+
+    private final int ON = 1;
+    private final int OFF = 0;
+
+    private static final String SECURE_KEY = "screen_off_udfps_enabled";
+
+    private AmbientDisplayConfiguration mAmbientConfig;
+    @UserIdInt
+    private final int mUserId;
+
+    public ScreenOffUdfpsPreferenceController(Context context, String key) {
+        super(context, key);
+        mUserId = UserHandle.myUserId();
+    }
+
+    public ScreenOffUdfpsPreferenceController setConfig(AmbientDisplayConfiguration config) {
+        mAmbientConfig = config;
+        return this;
+    }
+
+    private static boolean screenOffUdfpsAvailable(AmbientDisplayConfiguration config) {
+        return !TextUtils.isEmpty(config.udfpsLongPressSensorType());
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        // No hardware support for Screen-Off UDFPS
+        if (!screenOffUdfpsAvailable(getAmbientConfig())) {
+            return UNSUPPORTED_ON_DEVICE;
+        }
+
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return TextUtils.equals(getPreferenceKey(), "gesture_screen_off_udfps");
+    }
+
+    @Override
+    public boolean isPublicSlice() {
+        return true;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        return Settings.Secure.putInt(mContext.getContentResolver(), SECURE_KEY,
+                isChecked ? ON : OFF);
+    }
+
+    @Override
+    public boolean isChecked() {
+        return getAmbientConfig().screenOffUdfpsEnabled(mUserId);
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return -1;
+    }
+
+    private AmbientDisplayConfiguration getAmbientConfig() {
+        if (mAmbientConfig == null) {
+            mAmbientConfig = new AmbientDisplayConfiguration(mContext);
+        }
+        return mAmbientConfig;
+    }
+}
