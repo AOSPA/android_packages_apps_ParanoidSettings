9322bfb0f530d802b768cdd3b83a9c2896dc2e13
Michael Bestas <mkbestas@gmail.com>
2024-03-15 16:10:49 +0200

    ParanoidSettings: Make battery info configurable and enable it by default
    Change-Id: If38db229e420e9f7c0ac26abb42a2a309b2f87c2

--- src/co/aospa/settings/fuelgauge/ParanoidBatterySettingsFeatureProviderImpl.java
+++ src/co/aospa/settings/fuelgauge/ParanoidBatterySettingsFeatureProviderImpl.java
@@ -5,7 +5,17 @@
 
 package co.aospa.settings.fuelgauge;
 
+import android.content.Context;
+
 import com.android.settings.fuelgauge.BatterySettingsFeatureProviderImpl;
+import com.android.settings.R;
 
 /** Feature provider implementation for battery settings usage. */
-public class ParanoidBatterySettingsFeatureProviderImpl extends BatterySettingsFeatureProviderImpl {}
+public class ParanoidBatterySettingsFeatureProviderImpl extends BatterySettingsFeatureProviderImpl {
+
+    @Override
+    public boolean isBatteryInfoEnabled(Context context) {
+        return context.getResources().getBoolean(R.bool.config_show_battery_info);
+    }
+
+}
