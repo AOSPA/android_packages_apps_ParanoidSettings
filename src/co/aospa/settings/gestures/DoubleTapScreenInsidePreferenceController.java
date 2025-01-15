320ee1581f97dc42934c1ccac1cd6d3a663e7bbf
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-01-15 19:45:40 +0100

    ParanoidSettings: Cleanup DoubleTapScreenInsidePreferenceController
    Change-Id: Id602de3f1811af4f567435afe433d6e68920c0e8

--- src/co/aospa/settings/gestures/DoubleTapScreenInsidePreferenceController.java
+++ src/co/aospa/settings/gestures/DoubleTapScreenInsidePreferenceController.java
@@ -1,13 +1,12 @@
 /*
  * SPDX-FileCopyrightText: 2023 Yet Another AOSP Project
- * SPDX-FileCopyrightText: 2024 Paranoid Android
+ * SPDX-FileCopyrightText: 2024-2025 Paranoid Android
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package co.aospa.settings.gestures;
 
 import android.content.Context;
-import android.os.SystemProperties;
 import android.provider.Settings;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
@@ -15,9 +14,6 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
-import co.aospa.framework.preference.SecureSettingSwitchPreference;
-
-import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.widget.MainSwitchPreference;
@@ -26,6 +22,7 @@ public class DoubleTapScreenInsidePreferenceController extends AbstractPreferenc
         implements PreferenceControllerMixin, OnCheckedChangeListener {
 
     private static final String KEY = "gesture_double_tap_screen";
+    private static final String SECURE_KEY = Settings.Secure.DOZE_DOUBLE_TAP_GESTURE;
 
     private final Context mContext;
     private MainSwitchPreference mSwitch;
@@ -44,14 +41,6 @@ public class DoubleTapScreenInsidePreferenceController extends AbstractPreferenc
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mSwitch = screen.findPreference(getPreferenceKey());
-        mSwitch.setOnPreferenceClickListener(preference -> {
-            final boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
-                    Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 1) == 1;
-            Settings.Secure.putInt(mContext.getContentResolver(),
-                    Settings.Secure.DOZE_DOUBLE_TAP_GESTURE,
-                    enabled ? 0 : 1);
-            return true;
-        });
         mSwitch.addOnSwitchChangeListener(this);
         updateState(mSwitch);
     }
@@ -65,7 +54,7 @@ public class DoubleTapScreenInsidePreferenceController extends AbstractPreferenc
     @Override
     public void updateState(Preference preference) {
         final boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, 1) == 1;
+                SECURE_KEY, 0) == 1;
         setChecked(enabled);
     }
 
@@ -77,6 +66,6 @@ public class DoubleTapScreenInsidePreferenceController extends AbstractPreferenc
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         Settings.Secure.putInt(mContext.getContentResolver(),
-                Settings.Secure.DOZE_DOUBLE_TAP_GESTURE, isChecked ? 1 : 0);
+                SECURE_KEY, isChecked ? 1 : 0);
     }
 }
