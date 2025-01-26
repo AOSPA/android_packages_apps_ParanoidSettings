f1148f1e37b63d5bd072a56b1a9fe1beb0125868
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-01-26 19:13:15 +0100

    ParanoidSettings: Provide our custom BatterySettingsFeatureProviderImpl
    Change-Id: I75dff650320812a07e0e69f7c65abc6c2ffa5c63

---
+++ src/co/aospa/settings/fuelgauge/ParanoidBatterySettingsFeatureProviderImpl.kt
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.settings.fuelgauge
+
+import com.android.settings.fuelgauge.BatterySettingsFeatureProviderImpl
+
+/** Feature provider implementation for battery settings usage. */
+class ParanoidBatterySettingsFeatureProviderImpl : BatterySettingsFeatureProviderImpl() {}
