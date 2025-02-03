773bf3db445287d02f65e12ac9ffa1236e7099a1
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-02-03 13:14:23 +0100

    ParanoidSettings: Rewrite ST2W, DT2W and PU2W preference controllers in Kotlin
    Change-Id: I8c0af71e0deb1a942f6d4ef968ef2badc8f988b3

---
+++ src/co/aospa/settings/gestures/DoubleTapScreenInsidePreferenceController.kt
@@ -0,0 +1,47 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Yet Another AOSP Project
+ * SPDX-FileCopyrightText: 2024-2025 Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.settings.gestures
+
+import android.content.Context
+import android.provider.Settings
+import android.widget.CompoundButton
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.widget.MainSwitchPreference
+
+class DoubleTapScreenInsidePreferenceController(
+    context: Context
+) : AbstractPreferenceController(context), CompoundButton.OnCheckedChangeListener {
+
+    private var mainPreference: MainSwitchPreference? = null
+
+    override fun getPreferenceKey(): String = KEY
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        mainPreference = screen.findPreference(getPreferenceKey())
+        mainPreference?.addOnSwitchChangeListener(this)
+        mainPreference?.let{updateState(it as Preference)}
+    }
+
+    override fun updateState(preference: Preference) {
+        val enabled = Settings.Secure.getInt(mContext.contentResolver, SECURE_KEY, 0) == 1
+        mainPreference?.updateStatus(enabled)
+    }
+
+    override fun isAvailable(): Boolean = true
+
+    override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+        Settings.Secure.putInt(mContext.contentResolver, SECURE_KEY, if (isChecked) 1 else 0)
+    }
+
+    companion object {
+        private const val KEY = "gesture_double_tap_screen"
+        private const val SECURE_KEY = Settings.Secure.DOZE_DOUBLE_TAP_GESTURE
+    }
+}
