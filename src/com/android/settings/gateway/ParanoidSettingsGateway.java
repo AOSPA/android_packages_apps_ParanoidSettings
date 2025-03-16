9c3bff99fcc61a2c745a2b3c62f47f66020e6901
Fazil Sheik <fazil.sheik96@gmail.com>
2025-03-16 07:40:49 +0530

    ParanoidSettings: Host Smart Pause settings fragment in PASettingsActivity
    Change-Id: I6a06b9e8dd77955f60c69e139e3c3318045bb72c

--- src/com/android/settings/gateway/ParanoidSettingsGateway.java
+++ src/com/android/settings/gateway/ParanoidSettingsGateway.java
@@ -20,6 +20,7 @@ package com.android.settings.core.gateway;
 import co.aospa.settings.display.DcDimmingSettings;
 import co.aospa.settings.display.RefreshRateSettings;
 import co.aospa.settings.gestures.PowerButtonTorchSettings;
+import co.aospa.settings.sound.AdaptivePlaybackSoundSettings;
 
 public class ParanoidSettingsGateway {
 
@@ -31,5 +32,6 @@ public class ParanoidSettingsGateway {
         DcDimmingSettings.class.getName(),
         RefreshRateSettings.class.getName(),
         PowerButtonTorchSettings.class.getName(),
+        AdaptivePlaybackSoundSettings.class.getName(),
     };
 }
