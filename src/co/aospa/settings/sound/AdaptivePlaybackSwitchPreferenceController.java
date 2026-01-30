bea040b2892a4c891a2a7fc0d38bc026647270d1
Omkar Chandorkar <gotenksIN@aospa.co>
2026-01-30 21:18:34 +0900

    fixup! ParanoidSettings: AdaptivePlaybackSwitchPreferenceController: Refactor to use TogglePreferenceController
    Change-Id: I18c6281a4b253308adebebf046f9a1ea6d1dfa07
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- src/co/aospa/settings/sound/AdaptivePlaybackSwitchPreferenceController.java
+++ src/co/aospa/settings/sound/AdaptivePlaybackSwitchPreferenceController.java
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-package com.android.settings.sound;
+
+package co.aospa.settings.sound;
 
 import android.content.Context;
 import android.database.ContentObserver;
